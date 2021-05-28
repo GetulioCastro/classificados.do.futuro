7e86fe5e16f831dc52adf1aed0debe539797aedd
Getulio Castro <augutoge71@gmail.com>
2021-05-28 10:56:45 -0300

    Django iniciado

---
+++ classificados_do_futuro/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for classificados_do_futuro project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'classificados_do_futuro.settings')
+
+application = get_asgi_application()
